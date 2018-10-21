f241adb7eabe14a16e376908bbc6c88924245220
Noah Clements <noahnoah900@gmail.com>
2018-10-21 11:53:02 -0300

    initialized error and added multiplication operators
    compile bugs

--- TriangleBadge.java
+++ TriangleBadge.java
@@ -22,8 +22,7 @@ public class TriangleBadge {
 
 
 	public double getArea() {
-		double area;	// using Heron's formula to calculate the area
-		double s = (sideA + sideB + sideC) / 2; //semi-perimeter of the triangle
+		double area = 0;
 		if(sideA == sideB && sideB == sideC) {
 			// equilateral
 			area = ((Math.sqrt(3) / 4) * Math.pow(sideA, 2)); // formula for finding equilateral triangle area
@@ -31,11 +30,11 @@ public class TriangleBadge {
 		} else if(sideA == sideB || sideB == sideC || sideC == sideA) {
 			// isoceles
 			if(sideA == sideB) {
-				area = sideC * Math.sqrt((sideB*sideB)-(0.5(sideC*sideC)) / 2);
+				area = sideC * Math.sqrt((sideB*sideB)-(0.5*(sideC*sideC)) / 2);
 			}  else if(sideB == sideC) {
-				area = sideA * Math.sqrt((sideC*sideC)-(0.5(sideA*sideA)) / 2);
+				area = sideA * Math.sqrt((sideC*sideC)-(0.5*(sideA*sideA)) / 2);
 			}  else if(sideC == sideA) {
-				area = sideB * Math.sqrt((sideA*sideA)-(0.5(sideB*sideB)) / 2);
+				area = sideB * Math.sqrt((sideA*sideA)-(0.5*(sideB*sideB)) / 2);
 			}
 		} else if((sideA*sideA) + (sideB*sideB) == (sideC*sideC)) { // right-angle triangle if / else if statements
 			area = Math.sqrt((sideA*sideA) + (sideB*sideB));
