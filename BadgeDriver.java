c4e6082cd69c361f25ed5db1af0d19d2ac1a5490
Noah Clements <noahnoah900@gmail.com>
2018-10-21 19:09:41 -0300

    added comments

--- BadgeDriver.java
+++ BadgeDriver.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
-import java.text.NumberFormat;
+import java.text.DecimalFormat;
 public class BadgeDriver {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		NumberFormat format = NumberFormat.getNumberInstance();
-		format.setMaximumFractionDigits(3);
-
+		DecimalFormat format = new DecimalFormat(".000"); // max decimals = 3
 		double sideA, sideB, sideC, radius;
 		double area = 0;
-		double maxTriangle = 0;
+		double maxTriangle = 0;		// declaring and initializing variables 
 		double maxCircle = 0;
 		int choice = 0;
 
-		while(choice != 3) {
+		while(choice != 3) {	// loop this until user inputs 3
 
 			System.out.println("Select one of the following options:");
 			System.out.println("1 - Enter the information for a triangular badge.");
@@ -22,44 +20,44 @@ public class BadgeDriver {
 
 			choice = sc.nextInt();
 
-			if(choice == 1) {
+			if(choice == 1) {	// if user decides on a triangle badge, asks and waits for user to input all 3 sides
 				System.out.println("Length of side (cm): ");
 				sideA = sc.nextDouble();
 				System.out.println("Length of side (cm): ");
 				sideB = sc.nextDouble();
 				System.out.println("Length of side (cm): ");
 				sideC = sc.nextDouble();
-				TriangleBadge t1 = new TriangleBadge(sideA, sideB, sideC);
+				TriangleBadge t1 = new TriangleBadge(sideA, sideB, sideC);	// constructs a TriangleBadge with the 3 inputs
 
-				if(t1.getArea() == -1) {
+				if(t1.getArea() == -1) {	// getArea() returns -1 if the user inputs dont meet the requirements
 					System.out.println("Shape not available.");
 				} else {
-					System.out.println("Material: " + format.format(t1.getArea()));
-					System.out.println("Thread: " + format.format(t1.getPerimeter()));
+					System.out.println("Material: " + format.format(t1.getArea()) + "cm^2");	// prints out the area and perimeter
+					System.out.println("Thread: " + format.format(t1.getPerimeter()) + "cm");	// with the correct format
 					area = t1.getArea();
-					if(area > maxTriangle) {
+					if(area > maxTriangle) {	// used to determine the biggest triangle area as the loop goes
 						maxTriangle = area;
 					}
 				}
-			} else if(choice == 2) {
+			} else if(choice == 2) {	// if the user decides on a circle badge, asks and waits on user to input the radius
 				System.out.println("Length of radius (cm): ");
 				radius = sc.nextDouble();
-				CircleBadge c1 = new CircleBadge(radius);
-				System.out.println("Material: " + format.format(c1.getMaterialNeeded()));
-				System.out.println("Thread: " + format.format(c1.getThreadNeeded()));
+				CircleBadge c1 = new CircleBadge(radius);	// constructs a CircleBadge with the radius
+				System.out.println("Material: " + format.format(c1.getMaterialNeeded()) + "cm^2");	// prints out the area and circumference
+				System.out.println("Thread: " + format.format(c1.getThreadNeeded()) + "cm");		// with the correct format
 				area = c1.getMaterialNeeded();
-				if(area > maxCircle) {
+				if(area > maxCircle) {	// used to determine the biggest circle area as the loops goes
 					maxCircle = area;
 				}
 			} 
 				
 		}
-			if(maxCircle > maxTriangle) {
+			if(maxCircle > maxTriangle) {	// used to determine the biggest area out of the two variables
 				System.out.println("The badge that requires the most is a Circle");
-				System.out.println("The amount of material needed is: " + format.format(maxCircle));
+				System.out.println("The amount of material needed is: " + format.format(maxCircle) + "cm^2");
 			} else {
 				System.out.println("The badge that requires the most is a Triangle");
-				System.out.println("The amount of material needed is: " + format.format(maxTriangle));
+				System.out.println("The amount of material needed is: " + format.format(maxTriangle) + "cm^2");
 			}
 
 
--- TriangleBadge.java
+++ TriangleBadge.java
@@ -1,8 +1,21 @@
+/**
+	Represents a Triangle Badge class, used to find sides, area, and perimeter
+	@author Noah Clements 3585596
+*/
 public class TriangleBadge {
+	/** the sides of the triangle */
 	double sideA, sideB, sideC;
+	/** the extremely small number to compare to w/ absolute numbers */
 	final double TOLERANCE = 1E-14;
+	/** the height of the isosceles triangle */
 	double height = 0;
+	/** the base of the triangle*/
 	double halfBase = 0;
+	/** Constructs a Triangle Badge given 3 sides
+	@param sideAIn side A of the triangle in cm
+	@param sideBIn side B of the triangle in cm
+	@param sideCIn side C of the triangle in cm
+	*/
 	public TriangleBadge(double sideAIn, double sideBIn, double sideCIn) {
 		sideA = sideAIn;
 		sideB = sideBIn;
@@ -10,31 +23,39 @@ public class TriangleBadge {
 
 	}
 
-
+	/** Returns side A of the triangle
+	@return side A of the triangle
+	*/
 	public double getSideA() {
 		return sideA;
 	}
-
+	/** Returns side B of the triangle
+	@return side B of the triangle
+	*/
 	public double getSideB() {
 		return sideB;
 	}
-
+	/** Returns side C of the triangle
+	@return side C of the triangle
+	*/
 	public double getSideC() {
 		return sideC;
 	}
 
+	/** Returns the area of the triangle based on many if statements
+	@return the area of a triangle
+	*/
 
 	public double getArea() {
-		double area = 0;
-		if((Math.abs(sideA - sideB) < TOLERANCE) && (Math.abs(sideB - sideC) < TOLERANCE)) {
-			// equilateral
-			area = ((Math.sqrt(3) / 4) * Math.pow(sideA, 2)); // formula for finding equilateral triangle area
-											// could be either side to the power of 2, doesn't have to be sideA
+		double area = 0;	// initializing area
+		// EQUILATERAL TRIANGLE
+		if((Math.abs(sideA - sideB) < TOLERANCE) && (Math.abs(sideB - sideC) < TOLERANCE)) { // if sideA equals sideB, and sideB equals sideC
+			area = ((Math.sqrt(3) / 4) * Math.pow(sideA, 2)); // formula for finding equilateral triangle area, doesn't specifically have to be sideA
+			// ISOSCELES TRIANGLE, if sideA equals sideB, or if sideB equals sideC, or if sideC equals sideA
 		} else if(Math.abs(sideA - sideB) < TOLERANCE || Math.abs(sideB - sideC) < TOLERANCE || Math.abs(sideC - sideA) < TOLERANCE) {
-			// isoceles
-			if(Math.abs(sideA - sideB) < TOLERANCE) {
-				halfBase = sideC / 2;
-				height = Math.sqrt((sideA*sideA)-(halfBase*halfBase));
+			if(Math.abs(sideA - sideB) < TOLERANCE) {	// nested if statements to decide which formula to use (ie. which side minus the other)
+				halfBase = sideC / 2;	// the base of the triangle divided by 2, makes the next formula more clean
+				height = Math.sqrt((sideA*sideA)-(halfBase*halfBase));	// pythagorean theorem
 				area = (halfBase * height);
 			}  else if(Math.abs(sideB - sideC) < TOLERANCE) {
 				halfBase = sideA / 2;
@@ -45,18 +66,21 @@ public class TriangleBadge {
 				height = Math.sqrt((sideC*sideC)-(halfBase*halfBase));
 				area = (halfBase * height);
 			}
-		} else if(Math.abs(((sideA*sideA) + (sideB*sideB)) - (sideC*sideC)) < TOLERANCE) { // right-angle triangle if / else if statements
+			// RIGHT ANGLE TRIANGLES, basically checking if c^2=a^2+b^2 but with 3 checks based on inputs
+		} else if(Math.abs(((sideA*sideA) + (sideB*sideB)) - (sideC*sideC)) < TOLERANCE) { 
 			area = (sideA * sideB) / 2;
 		} else if(Math.abs(((sideA*sideA) + (sideC*sideC)) - (sideB*sideB)) < TOLERANCE) {
 			area = (sideA * sideC) / 2;
 		} else if(Math.abs(((sideB*sideB) + (sideC*sideC)) - (sideA*sideA)) < TOLERANCE) {
 			area = (sideB * sideC) / 2;
-		} else {													// if they dont dont match the dimensions of the available triangles, it returns -1
+		} else {	// if they dont dont match the dimensions of the available triangles, it returns -1
 			area = -1;
 		}
 		return area;
 	}
-
+	/** returns the perimeter of the triangle
+	@return the perimeter of the triangle
+	*/
 	public double getPerimeter() {
 		return sideA + sideB + sideC;
 	}
