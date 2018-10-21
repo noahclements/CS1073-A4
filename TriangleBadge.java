fcbb3d9fb54e00426c8c4a0e37802af3b63ce1ab
Noah Clements <noahnoah900@gmail.com>
2018-10-21 14:35:14 -0300

    equilateral if statement updated
    compile error also fixed

--- TriangleBadge.java
+++ TriangleBadge.java
@@ -25,7 +25,7 @@ public class TriangleBadge {
 
 	public double getArea() {
 		double area = 0;
-		if(sideA == sideB && sideB == sideC) {
+		if((Math.abs(sideA - sideB) < TOLERANCE) && (Math.abs(sideB - sideC) < TOLERANCE)) {
 			// equilateral
 			area = ((Math.sqrt(3) / 4) * Math.pow(sideA, 2)); // formula for finding equilateral triangle area
 											// could be either side to the power of 2, doesn't have to be sideA
@@ -38,11 +38,11 @@ public class TriangleBadge {
 			}  else if(Math.abs(sideC - sideA) < TOLERANCE) {
 				area = sideB * Math.sqrt((sideA*sideA)-(0.5*(sideB*sideB)) / 2);
 			}
-		} else if(Math.abs(((sideA*sideA) + (sideB*sideB)) - (sideC*sideC) < TOLERANCE)) { // right-angle triangle if / else if statements
+		} else if(Math.abs(((sideA*sideA) + (sideB*sideB)) - (sideC*sideC)) < TOLERANCE) { // right-angle triangle if / else if statements
 			area = Math.sqrt((sideA*sideA) + (sideB*sideB));
-		} else if(Math.abs(((sideA*sideA) + (sideC*sideC)) - (sideB*sideB) < TOLERANCE)) {
+		} else if(Math.abs(((sideA*sideA) + (sideC*sideC)) - (sideB*sideB)) < TOLERANCE) {
 			area = Math.sqrt((sideA*sideA) + (sideC*sideC));
-		} else if(Math.abs(((sideB*sideB) + (sideC*sideC)) - (sideA*sideA) < TOLERANCE)) {
+		} else if(Math.abs(((sideB*sideB) + (sideC*sideC)) - (sideA*sideA)) < TOLERANCE) {
 			area = Math.sqrt((sideB*sideB) + (sideC*sideC));
 		} else {													// if they dont dont match the dimensions of the available triangles, it returns -1
 			area = -1;
