3c609457e88f781f5501e74caefd364a0a6b5eff
Noah Clements <noahnoah900@gmail.com>
2018-10-21 12:45:43 -0300

    CircleBadge beta in driver

--- BadgeDriver.java
+++ BadgeDriver.java
@@ -9,7 +9,7 @@ public class BadgeDriver {
 		System.out.println("1 - Enter the information for a triangular badge.");
 		System.out.println("2 - Enter the information for a circular badge.");
 		System.out.println("3 - Quit.");
-		System.out.println("Enter your choice:");
+		System.out.print("Enter your choice:");
 		int choice = sc.nextInt();
 		if(choice == 1) {
 			System.out.println("Length of side (cm): ");
@@ -21,7 +21,14 @@ public class BadgeDriver {
 			TriangleBadge t1 = new TriangleBadge(sideA, sideB, sideC);
 			System.out.println("Material: " + t1.getArea());
 			System.out.println("Thread: " + t1.getPerimeter());
-
+		} else if(choice == 2) {
+			System.out.println("Length of radius (cm): ");
+			radius = sc.nextDouble();
+			CircleBadge c1 = new CircleBadge(radius);
+			System.out.println("Material: " + c1.getMaterialNeeded());
+			System.out.println("Thread: " + c1.getThreadNeeded());
+		} else if(choice == 3) {
+			System.out.println("Bye!");
 		}
 
 
