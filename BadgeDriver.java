81f5c9f2ab3d2e0703bae21529374c5745baa3fd
Noah Clements <noahnoah900@gmail.com>
2018-10-21 12:34:04 -0300

    added driver class, triangleBadge beta

---
+++ BadgeDriver.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class BadgeDriver {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		double sideA, sideB, sideC, radius;
+
+		System.out.println("Select one of the following options:");
+		System.out.println("1 - Enter the information for a triangular badge.");
+		System.out.println("2 - Enter the information for a circular badge.");
+		System.out.println("3 - Quit.");
+		System.out.println("Enter your choice:");
+		int choice = sc.nextInt();
+		if(choice == 1) {
+			System.out.println("Length of side (cm): ");
+			sideA = sc.nextDouble();
+			System.out.println("Length of side (cm): ");
+			sideB = sc.nextDouble();
+			System.out.println("Length of side (cm): ");
+			sideC = sc.nextDouble();
+			TriangleBadge t1 = new TriangleBadge(sideA, sideB, sideC);
+			System.out.println("Material: " + t1.getArea());
+			System.out.println("Thread: " + t1.getPerimeter());
+
+		}
+
+
+
+
+
+	}
+}
