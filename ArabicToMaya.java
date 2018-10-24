ca4061c4c21d20067a0eaa5e6c9a03f458314a5c
Noah Clements <noahnoah900@gmail.com>
2018-10-24 17:30:05 -0300

    initial commit
    forgot to do incremental commits, will update w/ comments and maybe bug fixes

--- ArabicToMaya.java
+++ ArabicToMaya.java
@@ -1,5 +1,50 @@
+import java.util.Scanner;
 public class ArabicToMaya {
-	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-	
+		System.out.println("Enter an Arabic number");
+		int input = sc.nextInt();
+		int remainder = 0;
+		String temp;
+		String output = "";
+		int base20num = 0;
+		while(input != 0) {
+			temp = "";
+			remainder = input % 20;
+			//base20num = remainder * Math.pow(20, p);
+			if (remainder !=0)
+			{
+				while(remainder != 0) {
+					if(remainder > 0) {
+						if(remainder >= 5) {
+							temp += "|";
+							remainder -= 5;
+						} else if(remainder < 5) {
+							temp += "O";
+							remainder -= 1;
+						}
+					}
+
+				}
+			} else {
+				temp += "x";
+			}
+						
+			input = input / 20;
+			
+			//p++;
+			output = "\n" + temp + output;
+			
+		}
+			System.out.println(output);
+			
+
+
+
+
+
+	}
+
+
 }
